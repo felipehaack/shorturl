8ba0a8efa2590e88b04cc5f2bce82bd63f43b409
Felipe Haack Schmitz <felipe.haackschmitz@gmail.com>
2017-04-30 22:52:46 -0300

    Added correct md5 to shortened url 2

--- payu-api/test/resources/seed.sql
+++ payu-api/test/resources/seed.sql
@@ -2,7 +2,7 @@
 # --- !Ups
 INSERT INTO urls (id, url, url_shortened, url_hashed, created, deleted) VALUES
   (1, 'http://www.google.com', '1', 'c4ca4238a0b923820dcc509a6f75849b', now(), NULL),
-  (2, 'http://www.google.com', '1', 'c4ca4238a0b923820dcc509a6f75849b', now(), now());
+  (2, 'http://www.google.com', '2', 'c81e728d9d4c2f636f067f89cc14862c', now(), now());
 
 # --- !Downs
 TRUNCATE TABLE urls RESTART IDENTITY CASCADE;
